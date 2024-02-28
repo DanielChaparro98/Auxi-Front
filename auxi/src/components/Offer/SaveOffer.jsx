664fc56c276d677cbc577be4873d0541b600b646
Daniel Chaparro <dchaparro@lucasian.com>
2024-02-28 15:48:55 -0500

    Creación formulario creación offerta

--- auxi/src/components/Offer/SaveOffer.jsx
+++ auxi/src/components/Offer/SaveOffer.jsx
@@ -1,5 +1,7 @@
+import { TextField } from "@mui/material";
 import React,{ useEffect, useState }  from "react";
 import { useJwt } from "react-jwt";
+import { Calendar } from 'primereact/calendar';
 
 const token = sessionStorage.getItem('token')
 
@@ -19,10 +21,25 @@ function Offer(){
         timeFinal: ''
     })
 
-    
-
+    const handleChange = (e) =>{
+        const { name, value } = e.target;
+        setOffer((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
 
     return(
-        <div></div>
+        <form>
+            <div className="offer-container">
+                <h2>Creación Oferta</h2>
+                <TextField id="standard-basic" label="Nombre" variant="standard" value={offer.name} onChange={handleChange}/>
+                <TextField id="standard-multiline-static" label="Descripción" multiline rows={4} variant="standard" value={offer.description} onChange={handleChange}/>
+                <Calendar id="buttondisplay" value={handleChange} maxDate={time} showIcon/>
+                <Calendar id="timeBegin" onChange={handleChange} timeOnly/>
+                <Calendar id="timeFinal" onChange={handleChange} timeOnly />
+            </div>
+        </form>
+
     )
 }
